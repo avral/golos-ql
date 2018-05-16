4baf88ac83d4353564a04f6c4394c36916973d2e
avral <qweasd_96@mail.ru>
2018-05-17 02:44:11 +0800

    fix and .dockerignore

---
+++ api/posts/types.py
@@ -0,0 +1,77 @@
+import graphene
+from graphene.relay import Node
+from graphene_mongo import MongoengineObjectType
+from graphene.types.generic import GenericScalar
+
+from models import (
+    Post as PostModel,
+    Comment as CommentModel
+)
+from utils import find_comments, find_images
+
+
+class Meta(graphene.ObjectType):
+    image = GenericScalar(first=graphene.Boolean())
+    app = GenericScalar()
+    location = GenericScalar()
+    tags = GenericScalar()
+
+    def resolve_tags(self, info, first=False):
+        return self.get('tags', [])
+
+    def resolve_image(self, info, first=False):
+        images = self.get('image', [])
+
+        if images:
+            return images[0] if first else images
+
+        return []
+
+    def resolve_app(self, info):
+        return self.get('app', 'undefined')
+
+    def resolve_location(self, info):
+        return self.get('location')
+
+
+class Comment(MongoengineObjectType):
+    class Meta:
+        model = CommentModel
+        interfaces = (Node,)
+
+
+class Stats(graphene.ObjectType):
+    posts_count = graphene.Int()
+
+    def resolve_posts_count(self, info):
+        return PostModel.objects.count()
+
+
+class Vote(graphene.ObjectType):
+    voter = graphene.String()
+
+    def resolve_voter(self, info):
+        return self['voter']
+
+
+class Post(MongoengineObjectType):
+    comments = graphene.List(Comment)
+    json_metadata = graphene.Field(Meta)
+    thumb = graphene.String()
+    active_votes = graphene.List(Vote)
+
+    class Meta:
+        model = PostModel
+        interfaces = (Node,)
+
+    def resolve_comments(self, info):
+        return find_comments(self)
+
+    def resolve_image(self, info):
+        return self.json_metadata['image'][0]
+
+    def resolve_json_metadata(self, info):
+        return self.json_metadata
+
+    def resolve_thumb(self, info):
+        return find_images(self.body, first=True)
