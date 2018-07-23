517b7606dad3441a7902b860aece67f56cb77e05
avral <qweasd_96@mail.ru>
2018-07-23 20:45:02 +0300

    Update GDP model fields

--- stats/models.py
+++ stats/models.py
@@ -1,11 +1,43 @@
 from mongoengine import DynamicDocument
 from mongoengine.fields import (
-    StringField, IntField, DictField,
-    DateTimeField, ObjectId
+    StringField, IntField, DateTimeField,
+    FloatField
 )
 
 
 class DGPModel(DynamicDocument):
-    average_block_size = IntField()
+    confidential_sbd_supply_symbol = StringField()
+    confidential_sbd_supply_value = IntField()
+    confidential_supply_symbol = StringField()
+    confidential_supply_value = IntField()
+    current_aslot = IntField()
+    current_reserve_ratio = IntField()
+    current_sbd_supply_symbol = StringField()
+    current_sbd_supply_value = FloatField()
+    current_supply_symbol = StringField()
+    current_supply_value = FloatField()
+    current_witness = StringField()
+    head_block_id = StringField()
+    head_block_number = IntField()
+    last_irreversible_block_num = IntField()
+    max_virtual_bandwidth = IntField()
+    maximum_block_size = IntField()
+    num_pow_witnesses = IntField()
+    participation_count = IntField()
+    recent_slots_filled = IntField()
+    sbd_interest_rate = IntField()
+    sbd_print_rate = IntField()
+    time = DateTimeField()
+    total_pow = IntField()
+    total_reward_fund_steem_symbol = StringField()
+    total_reward_fund_steem_value = FloatField()
+    total_reward_shares2 = IntField()
+    total_vesting_fund_steem_symbol = StringField()
+    total_vesting_fund_steem_value = FloatField()
+    total_vesting_shares_symbol = StringField()
+    total_vesting_shares_value = FloatField()
+    virtual_supply_symbol = StringField()
+    virtual_supply_value = FloatField()
+    vote_regeneration_per_day = IntField()
 
     meta = {'collection': 'dynamic_global_property_object'}
--- stats/types.py
+++ stats/types.py
@@ -1,24 +1,24 @@
 import graphene
-from graphene.types.generic import GenericScalar
+from graphene.relay import Node
+from graphene_mongo import MongoengineObjectType
 
 from post.models import CommentModel
 from stats.models import DGPModel
-# from utils import prepare_json
+
+
+class GDB(MongoengineObjectType):
+    class Meta:
+        description = 'dynamic_global_properties'
+        model = DGPModel
+        interfaces = (Node,)
 
 
 class Stats(graphene.ObjectType):
     posts_count = graphene.Int()
-    dynamic_global_properties = GenericScalar()
+    dynamic_global_properties = graphene.Field(GDB)
 
     def resolve_posts_count(self, info):
         return CommentModel.objects(depth=0).count()
 
     def resolve_dynamic_global_properties(self, info):
-        # FIXME Расписать все в модели
-
-        d = DGPModel.objects.first().__dict__
-
-        for _ in ["_cls", "_dynamic_lock", "_fields_ordered"]:
-                del d[_]
-
-        return d
+        return DGPModel.objects.first()
