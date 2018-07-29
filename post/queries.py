cde6b0609efbc0784424308dbe03bc3e4a1f3a2a
avral <qweasd_96@mail.ru>
2018-07-29 17:43:49 +0300

    refactoring post queries

--- post/queries.py
+++ post/queries.py
@@ -4,63 +4,31 @@ import graphene
 
 from post.types import Post, Comment
 from post.models import CommentModel
+from common.fields import CustomMongoengineConnectionField, CommentIdentifier
+from post.utils import qs_ab_filter
 
 
 class PostQuery(graphene.ObjectType):
-    posts = graphene.List(
-        Post,
-        # tag=graphene.String(),
-        category=graphene.String(),
-        author=graphene.String(),
-        page=graphene.Int(),
-    )
+    posts = CustomMongoengineConnectionField(Post)
+    post = graphene.Field(Post, identifier=CommentIdentifier())
 
-    post = graphene.Field(Post, identifier=graphene.String())
-
-    comments = graphene.List(
-        Comment,
-        page=graphene.Int(),
-    )
-
+    comments = CustomMongoengineConnectionField(Comment)
     comment = graphene.Field(Comment, identifier=graphene.String())
 
-    # TODO Набистать абстрактную пагинацию и фильтрацию
-    def resolve_posts(self, info, page=1,
-                      author=None, category=None, tag=None):
-        # {"tags":\[.*"ru--golos".*?\],
-        page_size = int(os.getenv('PAGINATION', 10))
-        offset = (page - 1) * page_size
-        q = {
-            "depth": 0  # Only posts
-        }
+    def resolve_posts(self, info, args):
+        qs = CommentModel.objects(depth=0)
 
-        if author:
-            q['author'] = author
-        if category:
-            q['category'] = category
-        # if tag:
-        #     q['json_metadata__contains'] = r'{"tags":\[.*"%s".*?\],' % tag
-
-        return list(
-           CommentModel.objects(**q)
-           .skip(offset)
-           .limit(page_size)
-        )
+        return qs_ab_filter(qs, args)
 
     def resolve_post(self, context, identifier=None):
         author, permlink = identifier[1:].split('/')
 
         return CommentModel.objects.get(author=author, permlink=permlink)
 
-    def resolve_comments(self, info, page=1):
-        page_size = int(os.getenv('PAGINATION', 10))
-        offset = (page - 1) * page_size
+    def resolve_comments(self, info, args):
+        qs = CommentModel.objects(depth__ne=0)
 
-        return list(
-           CommentModel.objects(depth__ne=0)
-           .skip(offset)
-           .limit(page_size)
-        )
+        return qs_ab_filter(qs, args)
 
     def resolve_comment(self, context, identifier=None):
         author, permlink = identifier[1:].split('/')
