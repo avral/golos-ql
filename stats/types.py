8ae08b986cd16d82048e63d96b45f0ea46c62774
avral <qweasd_96@mail.ru>
2018-08-13 21:28:42 +0300

    add payout stats

--- stats/types.py
+++ stats/types.py
@@ -13,12 +13,35 @@ class GDB(MongoengineObjectType):
         interfaces = (Node,)
 
 
-class Stats(graphene.ObjectType):
-    posts_count = graphene.Int()
+class BlockChain(graphene.ObjectType):
     dynamic_global_properties = graphene.Field(GDB)
+
+    def resolve_dynamic_global_properties(self, info):
+        return DGPModel.objects.first()
+
+
+class PostStats(graphene.ObjectType):
+    posts_count = graphene.Int()
+    total_payout = graphene.Int(category=graphene.String())
 
     def resolve_posts_count(self, info):
         return CommentModel.objects(depth=0).count()
 
-    def resolve_dynamic_global_properties(self, info):
-        return DGPModel.objects.first()
+    def resolve_total_payout(self, info, category=None):
+        qs = CommentModel.objects(depth=0)
+
+        if category:
+            qs = qs.filter(category=category)
+
+        return qs.sum('total_payout_value')
+
+
+class Stats(graphene.ObjectType):
+    blockchain = graphene.Field(BlockChain)
+    posts = graphene.Field(PostStats)
+
+    def resolve_posts(self, info):
+        return PostStats()
+
+    def resolve_blockchain(self, info):
+        return BlockChain()
