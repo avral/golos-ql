6e19215adfd6b4ac25a2b50bd82d94625b6bfd16
avral <qweasd_96@mail.ru>
2018-08-16 22:52:02 +0300

    add filtering by meta & sorting by created/TPV

--- post/queries.py
+++ post/queries.py
@@ -11,23 +11,36 @@ class Geometry(graphene.InputObjectType):
     coordinates = graphene.List(graphene.Float)
 
 
-class MetaFilter(graphene.InputObjectType):
+class PostOrderingEnum(graphene.Enum):
+    created_ASC = '-created'
+    created_DESC = 'created'
+    total_payout_value_ASC = '-total_payout_value'
+    total_payout_value_DESC = 'total_payout_value'
+
+
+class MetaFilterInput(graphene.InputObjectType):
     tags = graphene.List(graphene.String)
     app = graphene.String()
     # TODO Разобраться с фильтрацией location = Geometry()
 
 
 class PostQuery(graphene.ObjectType):
-    posts = CustomMongoengineConnectionField(Post, meta=MetaFilter())
     post = graphene.Field(Post, identifier=CommentIdentifier())
+    posts = CustomMongoengineConnectionField(Post,
+                                             meta=MetaFilterInput(),
+                                             orderBy=PostOrderingEnum())
 
     comments = CustomMongoengineConnectionField(Comment)
     comment = graphene.Field(Comment, identifier=graphene.String())
 
     def resolve_posts(self, info, args):
-        qs = CommentModel.objects(depth=0)
+        qs = CommentModel.objects(depth=0, removed=False)
 
         meta = args.get('meta', {})
+
+        if 'orderBy' in args:
+            qs = qs.order_by(args['orderBy'])
+
         tags = meta.get('tags')
         app = meta.get('app')
 
@@ -45,7 +58,7 @@ class PostQuery(graphene.ObjectType):
         return CommentModel.objects.get(author=author, permlink=permlink)
 
     def resolve_comments(self, info, args):
-        qs = CommentModel.objects(depth__ne=0)
+        qs = CommentModel.objects(depth__ne=0, removed=False)
 
         return qs_ab_filter(qs, args)
 
