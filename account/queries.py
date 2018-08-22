a181638e4740922db94a8d942f9a434d6790c051
avral <qweasd_96@mail.ru>
2018-08-23 01:26:00 +0300

    add meta notNull filter for account meta

--- account/queries.py
+++ account/queries.py
@@ -5,13 +5,30 @@ from account.models import AccountModel, AccountAuthorityModel
 from common.fields import CustomMongoengineConnectionField
 
 
+class AccountMetaFilterInput(graphene.InputObjectType):
+    not_null = graphene.String()
+
+
 class AccountQuery(graphene.ObjectType):
     account = graphene.Field(Account, name=graphene.String())
-    accounts = CustomMongoengineConnectionField(Account)
-    account_authority = graphene.Field(AccountAuthority, account=graphene.String())
+    accounts = CustomMongoengineConnectionField(Account,
+                                                meta=AccountMetaFilterInput())
+
+    account_authority = graphene.Field(AccountAuthority,
+                                       account=graphene.String())
 
     def resolve_accounts(self, info, args):
-        return AccountModel.objects()
+        qs = AccountModel.objects()
+
+        meta = args.get('meta', {})
+        not_null = meta.get('not_null')
+
+        if not_null:
+            qs = qs.filter(
+                __raw__={f'json_metadata.{not_null}': {'$exists': True}}
+            )
+
+        return qs
 
     def resolve_account(self, info, name):
         return AccountModel.objects(name=name).first()
